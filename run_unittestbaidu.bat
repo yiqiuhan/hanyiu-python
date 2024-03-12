ef1d608858de0398df7e3af5c798a3789920ea51
yiqiuhan <46773527+yiqiuhan@users.noreply.github.com>
2024-03-12 14:07:36 +0800

    是

---
+++ run_unittestbaidu.bat
@@ -0,0 +1,2 @@
+@echo on
+python "C:\ProgramData\Jenkins\.jenkins\workspace\pytest\unittestbaidu.py"
--- unittestex.py
+++ unittestex.py
@@ -1,10 +1,18 @@
 import unittest
 
-class Unitdemo(unittest.TestCase):
-    def test_login(self):
-        print('this is log in defitation')
-        self.assertEqual('123',"123", msg = "assert fail")
+class TestMath(unittest.TestCase):
+    # 测试用例
+    def test_add(self):
+        self.assertEqual(1 + 1, 2)
 
+    def test_subtract(self):
+        self.assertEqual(3 - 2, 1)
 
-if __name__ == "__main__":
-    unittest.main()
+# 创建测试套件 
+suite = unittest.TestSuite()
+suite.addTest(TestMath('test_add'))
+suite.addTest(TestMath('test_subtract'))
+
+# 创建测试运行器
+runner = unittest.TextTestRunner()
+runner.run(suite)
