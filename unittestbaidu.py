cee746d4dbe610d01ee0da4c88078424971b23b5
yiqiuhan <46773527+yiqiuhan@users.noreply.github.com>
2024-03-12 12:51:16 +0800

    Update unittestbaidu.py

--- unittestbaidu.py
+++ unittestbaidu.py
@@ -1,20 +1,19 @@
-from selenium import webdriver
-import unittest
-
-class Test(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.driver = webdriver.Edge()
-        self.driver.get("https://www.baidu.com")
-        self.title = self.driver.title
-        print(self.title)
-
-    
-    def test1(self):
-        self.assertEqual(self.title,"百度一下，你就知道")
-
+from selenium import webdriver  
+import unittest  
+  
+class Test(unittest.TestCase):  
+  
+    def setUp(self) -> None:  
+        self.driver = webdriver.Edge()  
+        self.driver.get("https://www.baidu.com")  
+        self.title = self.driver.title  
+        print(self.title)  
+  
+    def test1(self):  
+        self.assertEqual(self.title, "百度一下，你就知道")  
+  
     def tearDown(self) -> None:  
-        self.driver.quit() 
-
-if __name__ == "__main__":
+        self.driver.quit()  
+  
+if __name__ == "__main__":  
     unittest.main()
